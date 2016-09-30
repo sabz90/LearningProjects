bfa5d779586993d736b3b323ab31f7524c5fc9ee
Sabz <sabarish90@outlook.com>
2016-09-30 08:33:41 +0530

    Simple WPF application with data binding. First cut

---
+++ LearnWpf01/LearnWpf01/Framework/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LearnWpf01.Framework
+{
+    class RelayCommand : ICommand
+    {
+        Func<bool> _canExecute;
+        Action<object> _execute;
+
+        public RelayCommand(string name, Func<bool> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+             return _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
