c0b4e67c41bba1968b397e845642415ad851be49
Sabz <sabarish90@outlook.com>
2016-11-22 00:17:00 +0530

    Some more playing with data binding

---
+++ LearnWpf01.v2/LearnWpf01/Model/Developer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LearnWpf01.Model
+{
+    class Developer : IEmployee
+    {
+        public int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            
+            set
+            {
+                if(_age != value)
+                {
+                    _age = value;
+                    
+                    RaisePropertyChanged(nameof(Age));
+                }
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        public Sex _sex;
+        public Sex Sex
+        {
+            get
+            {
+                return _sex;
+            }
+            set
+            {
+                if (_sex != value)
+                {
+                    _sex = value;
+                    RaisePropertyChanged("Sex");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string property = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ LearnWpf01.v2/LearnWpf01/ViewModel/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWpf01.ViewModel
+{
+    class BaseViewModel
+    {
+        private EmployeeViewModel _evm;
+        public BaseViewModel(EmployeeViewModel evm)
+        {
+            _evm = evm;
+        }
+    }
+}
