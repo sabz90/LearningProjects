96c5c841935a4eab056b962788326b8acff7c4e6
Eganandan <SEganandan@ad.harman.com>
2016-11-22 15:50:25 +0530

    Caliburn First Cut, Binding a property and method to some controls in the view.

---
+++ CaliburnMicroApp/CaliburnMicroApp/AppBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace CaliburnMicroApp
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //When this boorstrapper is loaded, AppViewModel will be fired. 
+            //The corresponsing view AppView willbe displayed as it is set in the App.xaml
+            DisplayRootViewFor<AppViewModel>();
+        }
+    }
+}
---
+++ CaliburnMicroApp/CaliburnMicroApp/AppViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+
+namespace CaliburnMicroApp
+{
+    public class AppViewModel : PropertyChangedBase
+    {
+        private int _count = 50;
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                NotifyOfPropertyChange(() => Count);
+            }
+        }
+        public bool CanIncrementCount
+        {
+            get { return Count < 100; }
+        }
+        public void IncrementCount()
+        {
+            Count++;
+            NotifyOfPropertyChange(() => CanIncrementCount);
+        }
+    }
+}
